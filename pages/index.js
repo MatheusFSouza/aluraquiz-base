707342844954f11a6ea612cf9af7db2c5379b957
MatheusFSouza <fmatheus.souza164@gmail.com>
2021-01-28 02:20:07 -0300

    Push do terceiro dia da imersão react

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ import GitHubCorner from '../src/components/GitHubCorner';
 import QuizLogo from '../src/components/QuizLogo';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -16,7 +18,7 @@ import { useRouter } from 'next/router';
 //   background-position: center;
 // `;
 
-export const QuizContainer = styled.div`
+const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
@@ -52,19 +54,14 @@ export default function Home() {
                 console.log('Fazendo uma submissão por meio do react');
               }}
               >
-                <input
-                  onChange={function(infosDoEvento) {
-                    console.log(infosDoEvento.target.value);
-                    // State
-                    //name = infosDoEvento.target.value;
-                    setName(infosDoEvento.target.value);
-                  }} 
+                <Input
+                  onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
                   placeholder="Digite seu nome" 
+                  value={name}
                 />
-                <button type="submit" disabled={name.length === 0}>
-                  Jogar 
-                  {name}
-                </button>
+                <Button type="submit" disabled={name.length === 0}>
+                  {`Jogar ${name}`}
+                </Button>
               </form>
             </Widget.Content>
         </Widget>
