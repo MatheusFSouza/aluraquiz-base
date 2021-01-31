5f15c1d403f6986af466a2be73c783c68f3d93da
MatheusFSouza <fmatheus.souza164@gmail.com>
2021-01-31 03:57:03 -0300

    push do quinto e ultimo dia da imersão react

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,8 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
+import Link from '../src/components/Link';
+import { motion } from 'framer-motion';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -43,7 +45,16 @@ export default function Home() {
       </Head>
       <QuizContainer>
       <QuizLogo />
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: {opacity: 1, y: '0'},
+            hidden: {opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        >
             <Widget.Header>
               <h1>Genshin Impact Quiz</h1>
             </Widget.Header>
@@ -66,11 +77,39 @@ export default function Home() {
             </Widget.Content>
         </Widget>
 
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: {opacity: 1},
+            hidden: {opacity: 0},
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Content>
             <h1>Quizes da Galera</h1>
 
-            <p>lorem ipsum sit amet....</p>
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, '')
+                  .replace('https:', '')
+                  .replace('.vercel.app', '')
+                  .split('.');
+
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
           </Widget.Content>
         </Widget>
         <Footer />
